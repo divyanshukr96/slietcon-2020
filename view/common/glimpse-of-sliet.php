c70fa3ef59562f316d1b96a210899f56bf05a2ac
Divyanshu <divyanand96@gmail.com>
2019-10-13 01:00:10 +0530

    flimpse of sliet added

--- view/common/glimpse-of-sliet.php
+++ view/common/glimpse-of-sliet.php
@@ -7,6 +7,138 @@
     <div class="container pt-4">
         <div class="row">
 
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/0017.jpg" alt="" width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/58460596_595806790903214_1237269147562278912_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/008.jpg" alt="" width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/53900745_569442093539684_4302973462932094976_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/Yoga12018.jpg" alt="" width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/98.jpg" alt="" width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/96.jpg" alt="" width="100%">
+            </div>
+
+<!--            <div class="col-12 col-sm-6 col-md-4 col-lg-3">-->
+<!--                <img src="../../assets/images/glimpse/" alt="" width="100%">-->
+<!--            </div>-->
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/54254955_574541976363029_6237239614210310144_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/45163713_502155456935015_1679510896260415488_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/45199735_502155443601683_1381851215763079168_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+<!--            <div class="col-12 col-sm-6 col-md-4 col-lg-3">-->
+<!--                <img src="../../assets/images/glimpse/" alt="" width="100%">-->
+<!--            </div>-->
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/57049048_589158574901369_8544953682976833536_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/45248430_502155600268334_7381368354394603520_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/45179271_502155630268331_6852476516672995328_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/45367172_502594160224478_6893870754341847040_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/59831847_599649643852262_8122803301663113216_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/59394043_599063650577528_4707089010281938944_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/58374288_595326344284592_8257584430588100608_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/52588115_562944430856117_644525315246260224_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/59039656_598534830630410_875388736643268608_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/53582002_570165256800701_2983336871381172224_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/0023.png" alt="" width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/45370962_502588880225006_7546090460549218304_n.jpg" alt=""
+                     width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/0021.png" alt="" width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/99.png" alt="" width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/0018.png" alt="" width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/004.jpg" alt="" width="100%">
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <img src="../../assets/images/glimpse/005.jpg" alt="" width="100%">
+            </div>
         </div>
     </div>
 </section>
