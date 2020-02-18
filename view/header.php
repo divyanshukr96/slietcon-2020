b2aac094c604cb18678bc3060f2135c759b51527
Divyanshu <divyanand96@gmail.com>
2020-02-18 10:31:12 +0530

    date updation start

--- view/header.php
+++ view/header.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
           integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
           crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://unpkg.com/popper.js@1.14.5/dist/umd/popper.min.js"></script>
     <link rel="stylesheet" href="/assets/css/layout-old.css">
